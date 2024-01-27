0f03476bb87bf2ff5fd746c69a563a619287e747
Gene <TinderholmGene@gmail.com>
2024-01-27 15:59:26 -0600

    clean up duplication

--- static/cuda_wrapper_test.go
+++ static/cuda_wrapper_test.go
@@ -1,28 +1,18 @@
 package static
 
 import (
+	"cuda-example/common"
 	"fmt"
-	"math/rand"
 	"testing"
 )
 
 func BenchmarkCudaAdd(b *testing.B) {
-	slizeSize := 1024 * 1024 * 400
-	first := make([]float64, slizeSize)
-	second := make([]float64, slizeSize)
-	setToRandom(first)
-	setToRandom(second)
+	first, second := common.GetTestSlices()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		if err := cudaAdd(first, second); err != nil {
 			fmt.Println("got err: ", err)
 			b.FailNow()
 		}
-	}
-}
-
-func setToRandom(floats []float64) {
-	for i := 0; i < len(floats); i++ {
-		floats[i] = rand.Float64()
 	}
 }
